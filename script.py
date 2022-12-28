e2ab8d7713d538dfa3a9fa0992954fcb79676401
Greg Schuman <gcschuman@hotmail.com>
2022-12-28 14:23:46 -0800

    Added comments to state where formulas will be placed.

---
+++ script.py
@@ -0,0 +1,21 @@
+#Initial Variables:
+#Age
+#Gender Identity
+#Smoker 
+#Number of Children
+#Body Mass Index
+
+# Insurance Formula
+# Print out of insurance cost
+
+# Adding 4 years
+# Calculate new insurance cost
+# Change between new and old insurance costs
+
+# BMI Factor
+# Rewrite age to old age 
+# Add 3.1 to the BMI
+
+# Calculate new insurance cost
+# Change between new and old insurance cost
+
