c185f5f8677365462943538050d3073fdf048b13
Greg Schuman <gcschuman@hotmail.com>
2022-12-31 10:46:24 -0800

    Added new formula if the age is back to its original, but the BMI is increased by 3.1.

--- script.py
+++ script.py
@@ -24,9 +24,14 @@ change_in_insurance_cost = new_insurance_cost -insurance_cost
 
 # BMI Factor
 # Rewrite age to old age 
+age = 28
 # Add 3.1 to the BMI
+bmi += 3.1
 # Calculate new insurance cost
+new_insurance_cost = 250 * age - 128 * gender_identity + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 # Change between new and old insurance cost
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+print("The change in estimated insurance cost after increasing BMI by 3.1 is " + str(change_in_insurance_cost) + " dollars.")
 
 # Gender Identity Factor
 # Change gender identity variable
